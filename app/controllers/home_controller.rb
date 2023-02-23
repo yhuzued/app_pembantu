b1065997b0843b7e27dac5dc0728087c5a91dcc6
yhuzued <yusufmait@gmail.com>
2023-02-23 20:09:31 +0700

    Create repeat button task

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,21 @@ class HomeController < ApplicationController
     @repeated_tasks = RepeatedTask.all
     @uncompleted_tasks = Task.where(completed: false)
     @completed_tasks = Task.where(completed: true)
+  end
 
-    if today_task_empty?
-      @repeated_tasks.each do |task|
-        t1 = Task.create(title: task.title, completed: task.completed)
-        t1.save
-      end
+  def create_repeeated_task
+    @repeated_tasks = RepeatedTask.all
+
+    @repeated_tasks.each do |task|
+      t1 = Task.create(title: task.title, completed: task.completed)
+      t1.save
     end
+    redirect_to root_path
   end
 
   private
 
   def today_task_empty?
-    Task.where(created_at: Date.today).empty?
+    Task.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).empty?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :repeated_tasks
   resources :tasks
   root "home#dashboard"
+  post "/created_repeated_tasks", to: "home#create_repeeated_task"
 end
