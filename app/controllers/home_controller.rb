3f0e15aff110ccf132a9449a27ee0b4880689ee5
yhuzued <yusufmait@gmail.com>
2023-02-23 19:13:15 +0700

    Add task model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def dashboard
+    @task = Task.new
+    @uncompleted_tasks = Task.where(completed: false)
+    @completed_tasks = Task.where(completed: true)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/dashboard'
   resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#dashboard"
 end
