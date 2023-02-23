35cd2461e22b99ba92f6e881eea659f4ce7cd82d
yhuzued <yusufmait@gmail.com>
2023-02-23 19:29:46 +0700

    Auto created repeated tasks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,18 @@ class HomeController < ApplicationController
     @repeated_tasks = RepeatedTask.all
     @uncompleted_tasks = Task.where(completed: false)
     @completed_tasks = Task.where(completed: true)
+
+    if today_task_empty?
+      @repeated_tasks.each do |task|
+        t1 = Task.create(title: task.title, completed: task.completed)
+        t1.save
+      end
+    end
+  end
+
+  private
+
+  def today_task_empty?
+    Task.where(created_at: Date.today).empty?
   end
 end
